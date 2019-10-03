cf536d795158a987e4b597bc95a148367fe95d40
JasonZFHuang <jasonhuangzf@gmail.com>
2019-10-03 14:45:27 -0700

    added Jason Page

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace WebApplication3.Controllers
             return View();
         }
 
+        public IActionResult Jason()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
